3ffd93fbc6118d36818b0d04964c739e7d923ca2
NickRimerQ <58778313+NickRimerQ@users.noreply.github.com>
2020-12-29 15:07:38 +0300

    Add files via upload
    New Header

--- Header.h
+++ Header.h
@@ -2,24 +2,110 @@
 #include <iostream>
 using namespace std;
 
+
+template<class T>
+class Vector
+{
+	T* vector;
+	int Size;
+public:
+	Vector(int size = 2);
+	~Vector();
+	Vector(const Vector& temp);
+	int GetSize()
+	{
+		return Size;
+	}
+	bool operator==(const Vector& temp);
+	Vector operator=(const Vector& temp);
+	Vector operator+(const T& temp);
+	Vector operator-(const T& temp);
+	Vector operator*(const T& temp);
+	Vector operator+(const Vector& temp);
+	Vector operator-(const Vector& temp);
+	Vector operator*(const Vector& temp);
+	friend istream& operator>>(istream& stream, const Vector& temp)
+	{
+		for (int i = 0; i < temp.Size; i++)
+		{
+			stream >> temp.vector[i];
+		}
+		return stream;
+	}
+	friend ostream& operator<<(ostream& stream, const Vector& temp)
+	{
+		for (int i = 0; i < temp.Size; i++)
+		{
+			stream << temp.vector[i] << " ";
+		}
+		stream << endl;
+		return stream;
+	}
+	T& operator[](int index);
+};
+
+
+template<class T>
 class Matrix
 {
-	long int** arr;
+	T** arr;
 	int line;
 	int column;
 public:
+	Matrix();
 	Matrix(int line, int column);
 	~Matrix();
-	Matrix(const Matrix& other);
-	Matrix& operator = (const Matrix& other);
-	Matrix operator + (const Matrix& other);
-	Matrix operator - (const Matrix& other);
-	Matrix operator * (const Matrix& other);
-	Matrix operator * (const int other);
-	bool operator == (const Matrix& other);
-	bool operator != (const Matrix& other);
-	void filling();
-	void print();
+	Matrix(const Matrix& temp);
+	bool operator == (const Matrix& temp);
+	Matrix operator = (const Matrix<T>& temp);
+	Matrix operator + (const Matrix& temp);
+	Matrix operator - (const Matrix& temp);
+	Matrix operator * (const Matrix& temp);
+	friend istream& operator>>(istream& stream, Matrix& temp)
+	{
+		if (temp.arr != nullptr)
+		{
 
 
+			for (int i = 0; i < temp.line; i++) {
+				for (int j = 0; j < temp.column; j++) {
+					stream >> temp.arr[i][j];
+				}
+			}
+			return stream;
+		}
+		else
+		{
+			Vector<T> vector;
+
+			temp.arr = new T * [vector.GetSize()];
+
+			for (int i = 0; i < vector.GetSize(); i++)
+			{
+				temp.arr[i] = new T[vector.GetSize()];
+			}
+
+			temp.line = vector.GetSize();
+			temp.column = vector.GetSize();
+
+			for (int i = 0; i < vector.GetSize(); i++)
+			{
+				cin >> vector;
+				for (int j = 0; j < vector.GetSize(); j++)
+				{
+					temp.arr[i][j] = vector[j];
+				}
+			}
+		}
+	}
+	friend ostream& operator<< (ostream& stream, const Matrix& temp)
+	{
+		for (int i = 0; i < temp.line; i++) {
+			for (int j = 0; j < temp.column; j++) {
+				stream << temp.arr[i][j] << "\t";
+			}
+			stream << endl;
+		}
+		return stream;
+	}
 };
